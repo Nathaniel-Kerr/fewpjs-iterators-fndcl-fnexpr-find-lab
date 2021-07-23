658bf495c505b55635605348c1d4ceef7413f652
Nathaniel <NathanielKerr10@gmail.com>
2021-07-23 16:05:33 -0700

    commit tree 2016e4d4a1fda9a971e47c6f3d3f8b0f4e26369c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function superbowlWin(array) {
+    let win = array.find(s => s.result === "W");
+    if (win){
+        return win.year;
+    }
+    else {
+        return undefined;
+    }
+}
+
